5bf1e670523dc535df9ed4c6cddde52aa3a69644
Luca Milchrahm <2310653270@stud.fh-kufstein.ac.at>
2025-03-06 14:40:46 +0100

    step1 and step2 finished

---
+++ QuestionsAppSimple/QuestionsAppSimple/DB/Question.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QuestionsAppSimple.DB;
+
+public class Question
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    public string Content { get; set; } = "";
+    public int Votes { get; set; } = 0;
+}
---
+++ QuestionsAppSimple/QuestionsAppSimple/DB/QuestionsContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace QuestionsAppSimple.DB;
+
+public class QuestionsContext : DbContext
+{
+    public QuestionsContext(DbContextOptions options) : base(options)
+    {
+        
+    }
+    
+    public DbSet<Question> Questions { get; set; }
+    
+}
