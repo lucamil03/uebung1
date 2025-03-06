814da990bfea2330292a0ea93c606ff8a478c69e
Luca Milchrahm <2310653270@stud.fh-kufstein.ac.at>
2025-03-06 14:29:27 +0100

    step0 finished

--- QuestionsAppSimple/QuestionsAppSimple/Program.cs
+++ QuestionsAppSimple/QuestionsAppSimple/Program.cs
@@ -1,6 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Add Swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+// Enable swagger and SwaggerUI in Developer Mode.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.Run();
